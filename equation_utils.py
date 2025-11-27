85725c0a8f630bf75c3b9246a09ab7373bfd9e18
nouronallah <nour.onallah1234@gmail.com>
2025-11-27 18:33:55 +0300

    Implement formula splitting functions
    Added functions to split formulas based on uppercase letters and digits.

--- equation_utils.py
+++ equation_utils.py
@@ -1,4 +1,42 @@
-# Add the import statements for necessary sympy functions here
+def split_before_each_uppercases(formula):
+    if not formula:
+      return []
+    
+    split_formula = []
+    start = 0 
+    end = 1
+
+    while end < len(formula) :
+
+      
+      if formula[end].isupper() :
+        split_formula.append(formula[start:end])
+        start = end
+      
+      end += 1
+
+
+    split_formula.append(formula[start:end])
+
+    return split_formula
+    
+def split_at_first_digit(formula):
+    digit_location = 1
+
+    for char in formula[1 : ] :
+
+      if char.isdigit():
+        break
+
+      digit_location += 1
+
+    if digit_location == len(formula) :
+      return formula , 1
+
+    else :
+      prefix = formula [ :digit_location]
+      number = int (formula[digit_location: ])
+      return prefix,number
 
 
 ELEMENTS = [
